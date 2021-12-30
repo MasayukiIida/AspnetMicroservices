d934936bb3db2fa562602abd4545de42a343fdf6
MasayukiIida <beadaman.jobhunting@gmail.com>
2021-12-30 19:52:04 +0900

    finished section 2.

--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,3 +1,8 @@
+// <22.Developing...>
+using Catalog.API.Data;
+using Catalog.API.Repositories;
+// </22.Developing...>
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +11,11 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// <22.Developing...>
+builder.Services.AddScoped<ICatalogContext, CatalogContext>();
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
+// </22.Developing...>
 
 var app = builder.Build();
 
