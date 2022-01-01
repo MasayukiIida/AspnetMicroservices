866bafc192dc58f9446202f1c2c5064cb3dbfd4a
MasayukiIida <beadaman.jobhunting@gmail.com>
2022-01-01 21:45:09 +0900

    finished section 5.

---
+++ src/Class1.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+class Class1
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(RuntimeInformation.FrameworkDescription);
+    }
+}
